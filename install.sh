9c03805ec7af47505c0642f8acd799efa1367ed1
tomis007 <empty@empty.com>
2025-04-21 19:40:20 -0400

    updates

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "install dependencies"
-sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock alacritty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep
+sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock alacritty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep qt6-wayland qt5-wayland
 
 echo "setup dotfiles"
 cp -r ./dotfiles/.* $HOME
@@ -17,3 +17,6 @@ echo "setup greetd"
 sudo mkdir -p /etc/greetd/
 sudo cp ./etc/greetd/config.toml /etc/greetd/
 sudo systemctl enable greetd.service
+
+# start polkit
+systemctl --user enable --now hyprpolkitagent.service
