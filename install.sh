fff10330101f31e3a63af8223467ab79e2a13c6c
tomis007 <empty@empty.com>
2025-05-08 18:57:03 -0400

    updates

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ mkdir -p $HOME/AUR/hyprswitch && pushd $_
 git clone https://aur.archlinux.org/hyprswitch.git .
 makepkg
 sudo pacman -U hyprswitch-3.3.2-1-any.pkg.tar.zst
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 popd
 
 echo "setup greetd"
