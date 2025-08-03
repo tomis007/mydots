e05de343f97e66456a21c86aa9791b0ba3df24a9
tomis007 <13437332+tomis007@users.noreply.github.com>
2025-08-03 13:00:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 echo "install dependencies"
-sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock ghostty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep qt6-wayland qt5-wayland cargo hyprpolkitagent gtk4 base-devel git dunst libnotify ttf-hack fzf zsh otf-fira-mono otf-fira-sans otf-font-awesome nvidia ttf-jetbrains-mono-nerd
+sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock ghostty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep qt6-wayland qt5-wayland cargo hyprpolkitagent gtk4 base-devel git dunst libnotify ttf-hack fzf zsh otf-fira-mono otf-fira-sans otf-font-awesome nvidia ttf-jetbrains-mono-nerd pipewire pipewire-pulse pipewire-audio
 
 echo "setup dotfiles"
 cp -r ./dotfiles/.config $HOME
 cp ./dotfiles/{.bashrc,.vimrc,.zshrc,.tmux.conf} $HOME
 
-echo "setup greetd"
-sudo mkdir -p /etc/greetd/
-sudo cp ./etc/greetd/config.toml /etc/greetd/
-sudo systemctl enable --now greetd.service
+# sound
+systemctl --user enable --now pipewire-pulse.service
 
 # start polkit
 systemctl --user enable --now hyprpolkitagent.service
 
 sudo systemctl disable --now avahi-daemon
+
+echo "setup greetd"
+sudo mkdir -p /etc/greetd/
+sudo cp ./etc/greetd/config.toml /etc/greetd/
+sudo systemctl enable --now greetd.service
