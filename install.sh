25c3189dbae0e1637214292eb33185b196335f31
tomis007 <13437332+tomis007@users.noreply.github.com>
2025-08-03 12:52:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "install dependencies"
-sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock ghostty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep qt6-wayland qt5-wayland cargo hyprpolkitagent gtk4 base-devel git dunst libnotify ttf-hack fzf zsh otf-fira-mono otf-fira-sans otf-font-awesome nvidia
+sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock ghostty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep qt6-wayland qt5-wayland cargo hyprpolkitagent gtk4 base-devel git dunst libnotify ttf-hack fzf zsh otf-fira-mono otf-fira-sans otf-font-awesome nvidia ttf-jetbrains-mono-nerd
 
 echo "setup dotfiles"
 cp -r ./dotfiles/.config $HOME
