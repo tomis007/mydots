e48b995d4242c3f5e4bfe4ede852fdba43905bde
tomis007 <13437332+tomis007@users.noreply.github.com>
2025-08-03 12:18:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "install dependencies"
 sudo pacman -S greetd greetd-tuigreet vim tmux rofi-wayland hyprland hypridle hyprpaper hyprlock ghostty waybar ttf-jetbrains-mono ttf-font-awesome papirus-icon-theme base-devel rust uwsm gtk4-layer-shell ttf-firacode-nerd nautilus gammastep qt6-wayland qt5-wayland cargo hyprpolkitagent gtk4 base-devel git dunst libnotify ttf-hack fzf zsh otf-fira-mono otf-fira-sans otf-font-awesome nvidia
 
 echo "setup dotfiles"
-cp -r ./dotfiles/.* $HOME
+cp -r ./dotfiles/.config $HOME
+cp ./dotfiles/{.bashrc,.vimrc,.zshrc,.tmux.conf} $HOME
 
 echo "setup greetd"
 sudo mkdir -p /etc/greetd/
